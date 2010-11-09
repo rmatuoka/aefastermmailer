710cd8d9d0050f5c8bd3ee1cdc883a40adc0743f
Rafael Matuoka <rmatuoka@agenciaeconomica.com.br>
2010-11-09 09:18:41 -0200

    Ajuste remetente

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -3,7 +3,7 @@ class Newsletter < ActionMailer::Base
     #se precisar os parametros são Email.deliver_padrao(corpo, "Assunto",email para&nbsp;&nbsp; #enviar(destino), email para resposta)
     def enviar(corpo, assunto, email = "noreply@fasterm.com.br",responder = "noreply@fasterm.com.br")
         recipients email
-        from "Nautilus <noreply@fasterm.com.br>"
+        from "Fasterm <noreply@fasterm.com.br>"
         subject assunto
         reply_to responder
         body :corpo => corpo
